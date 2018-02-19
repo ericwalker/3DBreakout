acfd0b9a5eacb13c1df2549cd89a6d341872bd80
ericwalker <ericdavid78929@gmail.com>
2018-02-19 17:20:19 +0800

    change the ItemDrop probability

--- 3D Breakout 2017/Assets/Scripts/PauseMenu.cs
+++ 3D Breakout 2017/Assets/Scripts/PauseMenu.cs
@@ -3,65 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class PauseMenu : MonoBehaviour {
+	
+	public static bool GameIsPaused = false;
 
-	public GameObject PauseUI;
+	public GameObject pauseMenuUI;
 
-	private bool paused = false;
-//	private Vector3 PauseUIPos = new Vector3 (20, 0, 0);
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			FindObjectOfType<AudioManager>().Play("ClickSound");
+//			GameIsPaused = !GameIsPaused;
+			if (GameIsPaused) {
+				Resume ();
+			}
 
-
-	void Start(){
-		
-//		PauseUI.SetActive (false);
+			else {
+				Pause ();
+			}
+		} 
 	}
 
-	void Update(){
-		if (Input.GetButtonDown ("Pause")) {
-
-			paused = !paused;
-		}
-
-		// if press the "Gear" icon, which is pause button
-		if (paused) { 
-			PauseUI.transform.localPosition = new Vector3(0, 0, 0);
-//			PauseUI.SetActive (true);
-			Time.timeScale = 0; // stop all of the motion in the game
-		}
-
-		// if press the Resume button
-		if (!paused) {
-			PauseUI.transform.localPosition = new Vector3(100, 0, 0);
-//			PauseUI.SetActive (false);
-			Time.timeScale = 1;
-		}
-			
-	}
-
-	// if press the pause button..
+	// if press the pause button
 	public void Pause_Buttun(){
-		paused = true;
+		if (GameIsPaused) {
+			Resume ();
+		}
 
+		else {
+			Pause ();
+		}
 	}
 
 	public void Resume(){
-		paused = false;
+		pauseMenuUI.SetActive (false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+
+	void Pause(){
+		pauseMenuUI.SetActive (true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
 	}
 
 	public void Restart(){
-//		GM.lives = 3;
-//		GM.instance.ball_num = 0;
-//		GM.instance.bricks = 21;
-//		ScoreManager.score = 0;
+		Time.timeScale = 1f;
+		GameIsPaused = false;
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 
 	public void Option(){
-		
+
 	}
 
 	public void MainMenu(){
+		Time.timeScale = 1f;
+
+		GameIsPaused = false;
 		SceneManager.LoadScene("Menu");
 	}
 }
